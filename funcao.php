e66ebb86f9aa10318b376d7df29bf4806b2f9f19
Zittko <daniel.y.saiki@gmail.com>
2026-03-10 21:58:54 -0300

    atualizãção do repositótio

--- funcao.php
+++ funcao.php
@@ -1,14 +1,24 @@
 <?php 
 	$nome = "Armandino"; // variavel escopo de script
+	$ano = 2020;
 
-	function exibir(){
+	function exibir() {
 
 		$nome = "Josefina"; // variavel escopo de função
 
 		echo "Valor da variável dentro da função $nome";
 	}
 
-	exibir();
-	echo "<br/><br/> Valor da variável fora da função $nome";
+	function exibirano() {
+		GLOBAL $ano;
+		$ano++;
+		return $ano;
+	}
 
+	exibir();
+	echo "<br/><br/> Valor da variável fora da função $nome <br/><br/>";
+	echo "<br/> Ano: ".$ano;
+	echo "<br/> Ano: ".exibirano();
+	echo "<br/> Ano: ".exibirano();
+	echo "<br/> Ano: ".($ano + 1);
  ?>
