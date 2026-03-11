4cf94db96af836570a456d3f6495b0703eff386d
Zittko <daniel.y.saiki@gmail.com>
2026-03-10 21:47:32 -0300

    atualizãção do repositótio

--- funcao.php
+++ funcao.php
@@ -1,13 +1,13 @@
 <?php 
-	$nome = "Armandino";
+	$nome = "Armandino"; // variavel escopo de script
 
 	function exibir(){
 
-		$nome = "Josefina";
+		$nome = "Josefina"; // variavel escopo de função
 
 		echo "Valor da variável dentro da função $nome";
 	}
-	
+
 	exibir();
 	echo "<br/><br/> Valor da variável fora da função $nome";
 
