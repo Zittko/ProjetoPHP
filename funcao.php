07ec895161024a08109904b68f279fcbf47c1012
Zittko <daniel.y.saiki@gmail.com>
2026-03-10 21:43:05 -0300

    atualizãção do repositótio

--- funcao.php
+++ funcao.php
@@ -7,14 +7,8 @@
 
 		echo "Valor da variável dentro da função $nome";
 	}
-
-	function calc($n1, $n2) {
-		$resultado = $n1 + $n2;
-		echo "<br/><br/> Resultado é igual a: $resultado";
-	}
 	
 	exibir();
-	calc(5, 10);
 	echo "<br/><br/> Valor da variável fora da função $nome";
 
  ?>
