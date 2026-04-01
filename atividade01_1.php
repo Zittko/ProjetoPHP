009e24811003d1e5557d08b83276815d220a1d21
Zittko <daniel.y.saiki@gmail.com>
2026-03-31 21:51:14 -0300

    Atualização e finalização das atividades 1 e 2

--- atividade01_1.php
+++ atividade01_1.php
@@ -6,28 +6,31 @@
 	$exa = 0;
 	$rep = 0;
 	$somsal = 0;
-
-	for ($aluno=1; $aluno < 6; $aluno++) {
-		$somsal = $somsal + ($n1 + $n2);
-		echo "Aluno $aluno</br>";
-		echo "Nota 1: $n1</br>";
-		echo "Nota 2: $n2</br>";
-		echo "Media: $med</br>";
-		if ($med < 3) {
-			echo "ALUNO REPROVADO</br></br>";
-			$rep++;
-		} elseif ($med >= 3 && $med <=7) {
-			echo "ALUNO EM EXAME</br></br>";
-			$exa++;
-		} else {
-			echo "ALUNO APROVADO</br></br>";
-			$apr++;
+	if ($n1 < 0 || $n1 > 10 || $n2 < 0 || $n2 > 10) {
+		echo "Nota em formato errado (Nota foi imposta como menor que 0 ou maior que 10";
+	} else {
+		for ($aluno=1; $aluno < 6; $aluno++) {
+			$somsal = $somsal + ($n1 + $n2);
+			echo "Aluno $aluno</br>";
+			echo "Nota 1: $n1</br>";
+			echo "Nota 2: $n2</br>";
+			echo "Media: $med</br>";
+			if ($med < 3) {
+				echo "ALUNO REPROVADO</br></br>";
+				$rep++;
+			} elseif ($med >= 3 && $med <=7) {
+				echo "ALUNO EM EXAME</br></br>";
+				$exa++;
+			} else {
+				echo "ALUNO APROVADO</br></br>";
+				$apr++;
+			}
 		}
-	}
-	$medsal = $somsal / 12;
+		$medsal = $somsal / 12;
 
-	echo "Alunos aprovados: $apr</br>";
-	echo "Alunos em exame: $exa</br>";
-	echo "Alunos reprovados: $rep</br>";
-	printf("Media total da classe: %.2f", $medsal);
+		echo "Alunos aprovados: $apr</br>";
+		echo "Alunos em exame: $exa</br>";
+		echo "Alunos reprovados: $rep</br>";
+		printf("Media total da classe: %.2f", $medsal);	
+	}
 ?>
--- atividade01_2.php
+++ atividade01_2.php
@@ -1,19 +1,31 @@
 <?php 
-	$usermesa = 3;
-	$lgr = 5;
-	$m1ocu = false; $m2ocu = true; $m3ocu = false; $m4ocu = false; $m5ocu = false; 
-	$m6ocu = true; $m7ocu = true; $m8ocu = true; $m9ocu = true; $m10ocu = false;
-	$m11ocu = false; $m12ocu = true; $m13ocu = false; $m14ocu = true; $m15ocu = true;
-	$m16ocu = false; $m17ocu = true; $m18ocu = false; $m19ocu = false; $m20ocu = true;
-	$m21ocu = true; $m22ocu = true; $m23ocu = false; $m24ocu = false; $m25ocu = true; 
-	$m26ocu = false; $m27ocu = true; $m28ocu = true; $m29ocu = false; $m30ocu = true;
-	$m31ocu = false; $m32ocu = false; $m33ocu = false; $m34ocu = true; $m35ocu = false; 
-	$m36ocu = true; $m37ocu = true; $m38ocu = false; $m39ocu = false; $m40ocu = true;
-	while ($usermesa == 0 || $lgr == 0) {
-		for ($mesa=0; $mesa < 40 ; $mesa++) { 
-			for ($lgr = 0; $lgr < 6 ; $lgr++) { 
-				
+	$cadRes = 0;
+	do {
+		$mesaSec = 4;
+		$cadSec = 5;
+		$cadRes = $cadRes + $cadSec;
+		for ($mesa = 1; $mesa <= 40; $mesa++) { 
+			$mesOcu = false;
+			$cadOcu = 0;
+			echo "MESA $mesa </br>";
+			for ($cadeira = 1; $cadeira <= 6; $cadeira++) {
+				echo "CADEIRA $cadeira: ";
+				if ($mesa == $mesaSec) {
+					if ($cadeira <= $cadSec) {
+						echo "OCUPADA </br>";
+						$cadOcu++;
+					} else {
+						echo "NÃO OCUPADA </br>";
+					}
+				} else {
+					echo "NÃO OCUPADA </br>";
+				}
 			}
+			if ($cadOcu == 6) {
+				echo "MESA OCUPADA </br>";
+				$mesOcu = true;
+			}
+			echo "</br>";
 		}
-	}
+	} while ($mesa == 0 || $cadRes == 240);
  ?>
