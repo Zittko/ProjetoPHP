9d4bcdb18495cf1c427e2c0896ec4c6206980dc4
Zittko <daniel.y.saiki@gmail.com>
2026-04-07 20:22:18 -0300

    aula de funções

--- aula_funcao.php
+++ aula_funcao.php
@@ -1,21 +1,29 @@
 <?php 
+	
+	$numero = 30;
 
 	function somaValor() { // Função em php sem parâmetro
 		echo "Somando...";
-	}
-
-	function printNome($nome) { // Função com parâmetro
-		echo "Olá, $nome";
 	}
 
 	function somar($num1, $num2) {
 		return $num1 + $num2;
 	}
 
+	function printNome($nome) { // Função com parâmetro
+		echo "Olá, $nome";
+	}
+
+	if (is_numeric($numero)) {
+		echo "É numero";
+	} else {
+		echo "Não é número";
+	}
+	echo "<br>";
 	somaValor();
 	echo "</br>";
 	printNome("Etecia");
 	echo "</br>";
 	$soma = somar(10.5, 42.25);
-	echo "$soma";
+	echo "O resultado da soma é $soma";
  ?>
