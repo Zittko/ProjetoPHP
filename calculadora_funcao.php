60cee151891bf52c8f426cf705c49e9e737224d1
Zittko <daniel.y.saiki@gmail.com>
2026-04-07 22:09:10 -0300

    aula de funções

--- calculadora_funcao.php
+++ calculadora_funcao.php
@@ -9,10 +9,42 @@
 		return $num1 * $num2;
 	}
 	function dividir($num1, $num2) {
-		return $num1 / $num2;
+		if ($num2 != 0) {
+			return $num1 / $num2;
+		} else {
+			echo "Não divisível por 0";
+		}
 	}
 	function naoNumero() {
 		echo "Um ou mais valores digitados não são números";
+	}
+	function checkNum($num1, $num2) {
+		if (is_numeric($num1) && is_numeric($num2)) {
+			return true;
+		} else {
+			naoNumero();
+			return false;
+		}
+	}
+	function operador($option) {
+		if ($option == 1) {
+			return "soma";
+		} elseif ($option == 2) {
+			return "subtração";
+		} elseif ($option == 3) {
+			return "multiplicação";
+		} elseif ($option == 4) {
+			return "divisão";
+		} else {
+			return null;
+		}
+	}
+	function responder($num1, $num2, $option, $resp) {
+		if (checkNum($num1, $num2)) {
+			echo "A ";
+			echo (operador($option));
+			echo " de $num1 e $num2 é $resp";
+		}
 	}
 
 	echo "1 - Soma <br>
@@ -23,40 +55,24 @@
 
 	$num1 = 30;
 	$num2 = 10;
-	$option = 2;
+	$option = 4;
 
 	switch ($option) {
 		case 1:
-			if (is_numeric($num1) && is_numeric($num2)) {
-				$respSoma = somar($num1, $num2);
-				echo "$num1 + $num2 = $respSoma";
-			} else {
-				naoNumero();
-			}
+			$respSoma = somar($num1, $num2);
+			responder($num1, $num2, $option, $respSoma);
 			break;
 		case 2:
-			if (is_numeric($num1) && is_numeric($num2)) {
-				$respSub = subtrair($num1, $num2);
-				echo "$num1 - $num2 = $respSub";
-			} else {
-				naoNumero();
-			}
+			$respSub = subtrair($num1, $num2);
+			responder($num1, $num2, $option, $respSub);
 			break;
 		case 3:
-			if (is_numeric($num1) && is_numeric($num2)) {
-				$respMult = multiplicar($num1, $num2);
-				echo "$num1 x $num2 = $respMult";
-			} else {
-				naoNumero();
-			}
+			$respMult = multiplicar($num1, $num2);
+			responder($num1, $num2, $option, $respMult);
 			break;
 		case 4:
-		if (is_numeric($num1) && is_numeric($num2)) {
-				$respDiv = dividir($num1, $num2);
-				echo "$num1 / $num2 = $respDiv";
-			} else {
-				naoNumero();
-			}
+			$respDiv = dividir($num1, $num2);
+			responder($num1, $num2, $option, $respDiv);
 			break;
 		default:
 			echo "Opção não disponível";
