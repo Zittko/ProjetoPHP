1b0282743230c919d3255aab52590ec59339245a
Zittko <daniel.y.saiki@gmail.com>
2026-04-28 22:05:22 -0300

    ProjetoLoja

--- ProjetoLoja/index.php
+++ ProjetoLoja/index.php
@@ -9,14 +9,19 @@
 <body>
 	<form action="contato.php" method="post">
 		<label for="idName">Name:</label>
-		<input type="text" name="name" id="idName" placeholder="Type your name" maxlength="50">
-		<br><hr>
+		<input type="text" name="name" id="idName" 
+		placeholder="Type your name" maxlength="50" 
+		required autocomplete="off">
+		<br><br>
 		<label for="idEmail">Email:</label>
-		<input type="email" name="email" id="idEmail" placeholder="Type your email" maxlength="50">
-		<br><hr>
+		<input type="email" name="email" id="idEmail"
+		placeholder="Type your e-mail" maxlength="50"
+		autocomplete="off" required>
+		<br><br>
 		<label for="idMessage">Message:</label>
-		<textarea name="message" id="idMessage" placeholder="Send a message..."></textarea>
-		<br><hr>
+		<textarea name="message" id="idMessage" placeholder="Send a message..."
+		required></textarea>
+		<br><br>
 		<input type="submit" name="enviar" value="Submit">
 		<input type="reset" name="limpar" value="Clear">
 	</form>
