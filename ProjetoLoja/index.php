194d235a93dc8148c389449ff8da7025d8f7ebc1
Zittko <daniel.y.saiki@gmail.com>
2026-04-28 21:50:32 -0300

    ProjetoLoja

--- ProjetoLoja/index.php
+++ ProjetoLoja/index.php
@@ -7,7 +7,19 @@
 	<title>Home</title>
 </head>
 <body>
-	
+	<form action="contato.php" method="post">
+		<label for="idName">Name:</label>
+		<input type="text" name="name" id="idName" placeholder="Type your name" maxlength="50">
+		<br><hr>
+		<label for="idEmail">Email:</label>
+		<input type="email" name="email" id="idEmail" placeholder="Type your email" maxlength="50">
+		<br><hr>
+		<label for="idMessage">Message:</label>
+		<textarea name="message" id="idMessage" placeholder="Send a message..."></textarea>
+		<br><hr>
+		<input type="submit" name="enviar" value="Submit">
+		<input type="reset" name="limpar" value="Clear">
+	</form>
 	<script src="js/script.js"></script>
 </body>
 </html>
