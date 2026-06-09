8ebeff05e3a4fa9f767a074c57b87d73abfec372
Zittko <daniel.y.saiki@gmail.com>
2026-06-09 20:14:45 -0300

    bd conexao

---
+++ conecta_bd.php
@@ -0,0 +1,18 @@
+<?php 
+
+	$HOST = "localhost";
+	$USER = "user_dbloja";
+	$PWD = "admin123";
+	$DB = "dbLoja";
+
+	// mysqli_connect("localhost", "user_dbloja","admin123", "dbLoja");
+	$con = mysqli_connect($HOST, $USER, $PWD, $DB);
+
+	mysqli_close($con);
+	if (mysqli_connect_errno()) {
+		echo "Não foi possível conectar com o Banco de Dados" . mysqli_connect_error();
+	} else {
+		echo "Banco de Dados conectado com sucesso!!!";
+	}
+	
+ ?>
