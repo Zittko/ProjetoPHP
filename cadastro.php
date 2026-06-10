b41a6ad1f583dc262e645e86b50d6835c46791c1
Zittko <daniel.y.saiki@gmail.com>
2026-06-09 21:49:52 -0300

    bd conexao

--- cadastro.php
+++ cadastro.php
@@ -1,5 +1,4 @@
 <?php 
-
 	$HOST = "localhost";
 	$USER = "user_dbloja";
 	$PWD = "admin123";
@@ -11,9 +10,13 @@
 	if (mysqli_connect_errno()) {
 		echo "Não foi possível conectar com o Banco de Dados" . mysqli_connect_error();
 	} else {
-		// $sql = "INSERT INTO tbPessoa(nome, email, idade)
-		// VALUES('Etecia', 'sac@etecia.com', 20)";
-		// mysqli_query($con, $sql);
-		mysqli_close	
+		$sql = "INSERT INTO tbPessoa(nome, email, idade) VALUES('$_POST[nome]', '$_POST[email]', '$_POST[idade]')";
+		if (mysqli_query($con, $sql)) {
+		 	echo "Cadastrado com sucesso!!!";
+		} else {
+		 	echo "Erro ao cadastrar :(" . mysqli_error();
+		}
+		mysqli_close($con);
 	}
+
  ?>
---
+++ inserindoDados.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Inserindo Dados PHP</title>
+</head>
+<body>
+	<form action="cadastro.php" method="post">
+		<label for="email">Nome:</label>
+		<input type="text" name="nome">
+		<br><br>
+		<label for="email">Email:</label>
+		<input type="text" name="email">
+		<br><br>
+		<label for="email">Idade:</label>
+		<input type="text" name="idade">
+		<br><br>
+		<input type="submit" value="Cadastrar">
+	</form>
+</body>
+</html>
